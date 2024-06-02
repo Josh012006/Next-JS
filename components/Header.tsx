a9a59a74a6fa216790ce4fb5a8035365b555f880
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2024-06-02 16:53:29 +0000

    Improving some things

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,13 @@ import Link from "next/link";
 import { Zeyada } from "next/font/google";
 
 
+
 const font = Zeyada({ subsets: ["latin"], weight: '400' });
 
 function Header () {
     return(
         <header className={`grid grid-cols-12 items-center min-h-20 ${font.className} sticky z-50 top-0`}>
-            <h1 className="col-span-12 mx-auto my-2 lg:col-span-5 lg:m-2 font-bold text-4xl lg:pl-40">JM</h1>
+            <Link href='/' className="col-span-12 mx-auto my-2 lg:col-span-5 lg:m-2 font-bold text-4xl lg:pl-40">JM</Link>
             <nav className="col-span-12 lg:col-span-7 text-xl lg:text-2xl flex justify-around items-center my-3 lg:my-1">
                 <Link href="/#home" className="m-3 lg:m-5">Home</Link>
                 <Link href="/#about" className="m-3 lg:m-5">About me</Link>
