c55549c38c642913045ea34ca435561f382cae1e
Josué Saint-Martin Junior MONGAN <josuesmjr.mongan@gmail.com>
2024-07-01 13:06:49 +0000

    Some minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import About from "@/components/main/About";
 import Home from "@/components/main/Home";
 import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
-import Image from "next/image";
 
 function Main() {
     return (
